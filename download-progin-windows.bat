1996bb86c6ed0830974b0fc8c99b0814704a67e1
adhelaip <adhelaip@gmail.com>
2013-11-28 23:42:47 +0700

    update link download adela

--- download-progin-windows.bat
+++ download-progin-windows.bat
@@ -3,7 +3,7 @@ git clone https://github.com/pradita72/II3160--Pemrograman-Integratif-.git
 git clone https://oliviatan@bitbucket.org/oliviatan/18211014-dan-18211029.git
 git clone https://github.com/prama13/18211037.git
 git clone https://github.com/khairaniummah/BintangAdinandra.git
-git clone https://github.com/adhelaip/IPT-Assignments.git
+git clone https://github.com/adhelaip/ikape.git
 git clone https://github.com/ningenis/II3160-Progin-18211002-18211033.git
 git clone https://github.com/WidyaSTI/Web-Service.git
 git clone https://github.com/cahgantcore/Progint.git
